24f5ec5ce765567a999b1f3c15c495c540f7e837
Roberto dos Santos Marques da Silva <zarp_rmsilva@orizon.com.br>
2019-11-27 17:21:20 -0300

    Correcones

--- src/main/java/br/com/zarpsystem/springrestprometheus/SpringRestPrometheusApplication.java
+++ src/main/java/br/com/zarpsystem/springrestprometheus/SpringRestPrometheusApplication.java
@@ -1,11 +1,14 @@
 package br.com.zarpsystem.springrestprometheus;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class SpringRestPrometheusApplication extends SpringBootServletInitializer {
 
 		public static void main(String[] args) {
