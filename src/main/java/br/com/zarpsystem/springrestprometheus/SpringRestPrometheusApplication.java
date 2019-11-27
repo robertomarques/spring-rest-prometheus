9e2b0982ef2bd2e93a28546b3abd4f59d3e3b2b5
Roberto dos Santos Marques da Silva <zarp_rmsilva@orizon.com.br>
2019-11-27 16:50:46 -0300

    Spring Boot to war

--- src/main/java/br/com/zarpsystem/springrestprometheus/SpringRestPrometheusApplication.java
+++ src/main/java/br/com/zarpsystem/springrestprometheus/SpringRestPrometheusApplication.java
@@ -2,12 +2,24 @@ package br.com.zarpsystem.springrestprometheus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringRestPrometheusApplication {
+public class SpringRestPrometheusApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestPrometheusApplication.class, args);
+		public static void main(String[] args) {
+			SpringApplication.run(SpringRestPrometheusApplication.class, args);
+		}
+
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+			return builder.sources(SpringRestPrometheusApplication.class);
+		}
 	}
 
-}
+/*	public static void main(String[] args) {
+		SpringApplication.run(SpringRestPrometheusApplication.class, args);
+	}*/
+
+
