75f0cf7e1625a467cc492258a74aaa25f59669aa
Roberto dos Santos Marques da Silva <zarp_rmsilva@orizon.com.br>
2019-11-28 17:24:24 -0300

    Tracing Driver

--- src/main/java/br/com/zarpsystem/springrestprometheus/DataSourceConfig.java
+++ src/main/java/br/com/zarpsystem/springrestprometheus/DataSourceConfig.java
@@ -12,8 +12,9 @@ public class DataSourceConfig {
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:test");
+        //dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.driverClassName("io.opentracing.contrib.jdbc.TracingDriver");        
+        dataSourceBuilder.url("jdbc:tracing:h2:mem:test?traceWithActiveSpanOnly=true");
         dataSourceBuilder.username("SA");
         dataSourceBuilder.password("");
         return dataSourceBuilder.build();
